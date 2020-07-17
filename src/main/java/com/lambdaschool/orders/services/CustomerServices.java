c884de4fb96cd9adb8f666187cd9fbe9adb9635b
karina-rodriguez <karminer60@gmail.com>
2020-07-16 18:58:43 -0700

    transferring code from part one. Almost done with Tuesday's requirements

---
+++ src/main/java/com/lambdaschool/orders/services/CustomerServices.java
@@ -0,0 +1,22 @@
+package com.lambdaschool.orders.services;
+
+import com.lambdaschool.orders.models.Agent;
+import com.lambdaschool.orders.models.Customer;
+import com.lambdaschool.orders.views.Orders;
+
+import java.util.List;
+
+public interface CustomerServices
+{
+    Customer save(Customer customer);
+
+    List<Customer> findAllCustomers();
+
+    Customer findById(long id);
+
+    Customer findByNameLike(String subname);
+
+    List<Orders> getOrders();
+
+
+}
